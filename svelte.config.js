af95033fb4109746f6430afeda3289364e392ec9
sinYa Iwasaki <iwasaki@sinproject.net>
2023-11-24 17:51:32 +0900

    Build and release on GitHub Pages #4

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import fs from 'fs'
 import { fileURLToPath } from 'url'
@@ -7,17 +7,18 @@ const file = fileURLToPath(new URL('package.json', import.meta.url))
 const json = await fs.promises.readFile(file, 'utf8')
 const pkg = JSON.parse(json)
 
+const base_path = process.env['CI'] ? '/babylonjs-sveltekit' : ''
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			precompress: true,
+		}),
+		paths: {
+			base: base_path,
+		},
 		version: {
 			name: pkg.version,
 		},
